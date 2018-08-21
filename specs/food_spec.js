f05d33acada59cebb11da9de54c474d3f1e6ee31
Kaspars Dzerins <kadzer@gmail.com>
2018-08-21 23:02:13 +0100

    added replenishment to Food

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -7,10 +7,14 @@ const Food = require('../food.js')
 
 describe('Food', function(){
   beforeEach(function(){
-    food = new Food('chicken');
+    food = new Food('chicken', 25);
   });
   it('should have a name', function(){
     const actual = food.name;
     assert.strictEqual(actual, 'chicken')
+  });
+  it('should have replenishment value', function(){
+    const actual = food.replenish;
+    assert.strictEqual(actual, 25)
   });
 })
